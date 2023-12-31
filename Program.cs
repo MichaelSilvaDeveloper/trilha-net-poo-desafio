1622867d00caa4e327462f991b18021660384a39
Michael Silva <michael.silva2000@outlook.com.br>
2023-12-31 12:10:19 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone("123456", "Modelo 1", "111111111", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
